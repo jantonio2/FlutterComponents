39c2f6c68a6a8d52911f14af124867852a9dadd0
jantonio2 <jesusrochavedia@gmail.com>
2022-08-18 11:26:49 -0400

    Utilizando Card Widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -6,10 +7,26 @@ class CardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Widget'),
       ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: [
+          Card(
+            child: Column(
+              children: const [
+                ListTile(
+                  leading: Icon(Icons.photo_album_outlined, color: AppTheme.primary),
+                  title: Text('Soy un título'),
+                  subtitle: Text('Non aute id deserunt pariatur exercitation nulla. Fugiat eiusmod veniam quis adipisicing. Consectetur aute esse elit minim ad irure. Aliqua occaecat qui sint consectetur.'),
+                )
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 
