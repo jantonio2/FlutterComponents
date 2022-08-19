2542793af5b3a02bb1dd150d0f1b8e82eccdc7fa
jantonio2 <jesusrochavedia@gmail.com>
2022-08-19 14:34:36 -0400

    Dandole mas estilo a la imagen del custon card 2

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 // import 'package:transparent_image/transparent_image.dart';
 
@@ -7,13 +8,19 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10,
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18)
+      ),
+      elevation: 30,
+      shadowColor: AppTheme.primary.withOpacity(0.6),
       child: Column(
-        children: const [
-          FadeInImage(
+        children: [
+          const FadeInImage(
             image: NetworkImage('https://img.freepik.com/premium-vector/meadows-landscape-with-mountains-hill_104785-943.jpg?w=2000'),
             placeholder: AssetImage('assets/carga.jpg'),
-            fadeInDuration: Duration( milliseconds: 200 ),
+            fadeInDuration: Duration( milliseconds: 300 ),
+            width: double.infinity,
             height: 250.0,
             fit: BoxFit.cover
           ),
@@ -21,6 +28,11 @@ class CustomCardType2 extends StatelessWidget {
             image: ('https://img.freepik.com/premium-vector/meadows-landscape-with-mountains-hill_104785-943.jpg?w=2000'),
             placeholder: kTransparentImage
           ),*/
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            child: const Text('Un hermoso paisaje')
+          )
         ],
       )
     );
