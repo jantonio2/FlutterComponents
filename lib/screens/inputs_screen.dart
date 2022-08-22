959a38681953950a2040e7171b3ed8ab26c1e2e4
jantonio2 <jesusrochavedia@gmail.com>
2022-08-22 17:20:53 -0400

    Creando dropdown y utilizandolo en el formulario

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -39,7 +39,21 @@ class InputsScreen extends StatelessWidget {
 
                 CustomInputField(labelText: 'Contraseña', hintText: '*****', obscureText: true, formProperty: 'password', formValues: formValues),
                 const SizedBox(height: 30),
-
+                
+                DropdownButtonFormField<String>(
+                  items: const [
+                    DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                    DropdownMenuItem(value: 'Superuser', child: Text('Superuser')),
+                    DropdownMenuItem(value: 'Developer', child: Text('Developer')),
+                    DropdownMenuItem(value: 'Jr. Developer', child: Text('Jr. Developer'))
+                  ],
+                  onChanged: (value) {
+                    print(value);
+                    formValues['role'] = value ?? 'Admin';
+                  },
+                ),
+                const SizedBox(height: 30),
+                
                 ElevatedButton(
                   child: const SizedBox(
                     width: double.infinity,
